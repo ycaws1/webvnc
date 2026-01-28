e597da92a7c7424b608748e7f4f5a245cf6200e9
Yee Chern Goh <yeecherngoh@Yees-MacBook-Pro.local>
2026-01-28 12:05:52 +0800

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apt-get python3 python3-pip
+apt-get python3-pip
 pip3 install playwright playwright-stealth asyncio asyncpg httpx python-dotenv pydantic apscheduler pywebpush
 # playwright install-deps chromium
 playwright install chromium
