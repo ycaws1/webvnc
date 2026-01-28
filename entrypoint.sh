c36c080b3b9fe145e0c8a80b2852457cc8b4537a
Yee Chern Goh <yeecherngoh@Yees-MacBook-Pro.local>
2026-01-28 09:24:50 +0800

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+rm -rf /tmp/.X*-lock /tmp/.X11-unix
+mkdir -p /root/.vnc
+echo 'password' | vncpasswd -f > /root/.vnc/passwd
+chmod 600 /root/.vnc/passwd
+vncserver :1 -geometry 1280x800 -depth 24
+/usr/share/novnc/utils/launch.sh --vnc localhost:5901 --listen 6080
